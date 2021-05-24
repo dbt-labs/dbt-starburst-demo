2d325cfa36af4334a4b6290165c390378feadae1
Jon Natkins <jon.natkins@fishtownanalytics.com>
2021-05-24 21:57:56 +0000

    Add sources.yml and stg_calendar model

---
+++ models/staging/airbnb/stg_calendar.sql
@@ -0,0 +1,17 @@
+{{ config(materialized='view') }}
+
+with renamed as (
+
+select
+  listing_id            as listing_id,
+  to_date(date)         as availability_date,
+  boolean(available)    as is_available,
+  price                 as price_dollars,
+  adjusted_price        as adjusted_price_dollars,
+  minimum_nights        as minimum_nights,
+  maximum_nights        as maximum_nights
+from {{ source('airbnb','calendar') }}
+
+)
+
+select * from renamed
