b4af6153392fffcc7262c23bb1b76a06b974d26b
Monica Miller <33696269+monimiller@users.noreply.github.com>
2022-10-12 16:23:11 -0500

    create hospital_beds staging model

--- models/staging/aws_covid_data_lake/stg_enigma_jhu.sql
+++ models/staging/aws_covid_data_lake/stg_enigma_jhu.sql
@@ -1,6 +1,6 @@
 with source as (
 
-    select * from {{ source('covid', 'enigma_jhu') }}
+    select * from {{ source('covid', 'hospital_beds') }}
 
 ),
 
---
+++ models/staging/aws_covid_data_lake/stg_hospital_beds.sql
@@ -0,0 +1,39 @@
+with source as (
+
+    select * from {{ source('covid', 'hospital_beds') }}
+
+),
+
+renamed as (
+
+    select
+    
+        objectid,
+        hospital_name,
+        hospital_type,
+        hq_address,
+        hq_address1,
+        hq_city,
+        hq_state,
+        hq_zip_code,
+        county_name,
+        state_name,
+        state_fips,
+        cnty_fips,
+        fips,
+        num_licensed_beds,
+        num_staffed_beds,
+        num_icu_beds,
+        adult_icu_beds,
+        peds_icu_beds,
+        bed_utilization,
+        avg_ventilator_usage,
+        potential_increase_in_bed_capac,
+        latitude,
+        longitude
+
+    from source
+
+)
+
+select * from renamed
