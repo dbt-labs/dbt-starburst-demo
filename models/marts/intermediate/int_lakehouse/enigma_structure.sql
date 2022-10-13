d4615d352eec7f8501bd36a056c40e60b2a3f816
Monica Miller <33696269+monimiller@users.noreply.github.com>
2022-10-13 17:10:15 -0500

    try new structure table

---
+++ models/marts/intermediate/int_lakehouse/enigma_structure.sql
@@ -0,0 +1,26 @@
+
+with enigma as (
+    select * from {{ ref('stg_enigma_jhu') }}
+
+),
+
+
+final as (
+
+
+SELECT
+    fips,
+    admin2,
+    province_state,
+    country_region,
+    confirmed
+FROM
+    enigma
+WHERE
+    country_region = 'US'
+    and province_state not like ''
+    and last_update = '2020-05-30T02:32:48'
+
+)
+
+select * from final
