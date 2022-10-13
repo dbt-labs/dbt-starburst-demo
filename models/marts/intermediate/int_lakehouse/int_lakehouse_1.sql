57e7ded13e1f5e8c421d0b6876fe9eaeaf0d4db0
Monica Miller <33696269+monimiller@users.noreply.github.com>
2022-10-13 15:28:23 -0500

    update query

--- models/marts/intermediate/int_lakehouse/int_lakehouse_1.sql
+++ models/marts/intermediate/int_lakehouse/int_lakehouse_1.sql
@@ -3,41 +3,41 @@ with enigma as (
 
 ),
 
-with cases as (
-    SELECT
-        fips,
-        admin2 AS county,
-        province_state,
-        country_region,
-        confirmed,
-        first_value(last_update) OVER (
-            PARTITION BY fips ORDER BY last_update DESC) AS most_recent,
-        last_update
-    FROM
-        enigma
-),
 
 final as (
 
-    SELECT
-        fips,
-        county,
-        province_state,
-        country_region,
-        confirmed,
-        last_update
-    FROM
-        cases
-    WHERE
-        last_update = most_recent
-    GROUP BY
-        fips,
-        county,
-        province_state,
-        country_region,
-        confirmed,
-        last_update;
-
+WITH
+    cases AS (
+        SELECT
+            fips,
+            admin2 AS county,
+            province_state,
+            country_region,
+            confirmed,
+            first_value(last_update) OVER (
+              PARTITION BY fips ORDER BY last_update DESC) AS most_recent,
+            last_update
+        FROM
+            enigma_jhu
+    )
+SELECT
+    fips,
+    county,
+    province_state,
+    country_region,
+    confirmed,
+    last_update
+FROM
+    cases
+WHERE
+    last_update = most_recent
+GROUP BY
+    fips,
+    county,
+    province_state,
+    country_region,
+    confirmed,
+    last_update;
 )
 
 select * from final
