50fdb607ff494ce0e1528fb2b6cd12a73c516d64
Monica Miller <33696269+monimiller@users.noreply.github.com>
2022-10-16 16:27:58 -0500

    as

--- models/marts/intermediate/int_aws/int_lakehouse_1.sql
+++ models/marts/intermediate/int_aws/int_lakehouse_1.sql
@@ -10,27 +10,28 @@ WITH
     cases AS (
         SELECT
             fips,
-            county,
+            admin2 AS county,
             province_state,
-            country_region,
+            replace(country_region, 'US', 'UNITED STATES') as country_region,
             confirmed,
             first_value(last_update) OVER (
               PARTITION BY fips ORDER BY last_update DESC) AS most_recent,
             last_update
         FROM
-            enigma
+            enigma_jhu
     )
 SELECT
     fips,
     county,
     province_state,
-    country_region,
+    UPPER(country_region) as country,
     confirmed,
     last_update
 FROM
     cases
 WHERE
     last_update = most_recent
+    and UPPER(country_region) != 'UNITED STATES'
 GROUP BY
     fips,
     county,
