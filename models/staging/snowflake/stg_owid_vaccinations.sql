4a45bd7afedbba8a9dc1c267ac8266cafaea55f1
Monica Miller <33696269+monimiller@users.noreply.github.com>
2022-10-16 15:03:59 -0500

    add snowflake land layer

---
+++ models/staging/snowflake/stg_owid_vaccinations.sql
@@ -0,0 +1,33 @@
+with source as (
+
+    select * from {{ source('snowflake', 'owid_vaccinations') }}
+
+),
+
+renamed as (
+
+    select
+    
+        date,
+        country_region,
+        iso3166_1,
+        total_vaccinations,
+        people_vaccinated,
+        people_fully_vaccinated,
+        daily_vaccinations_raw,
+        daily_vaccinations,
+        total_vaccinations_per_hundred,
+        people_vaccinated_per_hundred,
+        daily_vaccinations_per_million,
+        vaccines,
+        last_observation_date,
+        source_name,
+        source_website,
+        last_update_date,
+        last_reported_flag
+
+    from source
+
+)
+
+select * from renamed
