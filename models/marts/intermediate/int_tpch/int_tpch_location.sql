0eaeca909e3b77064cfd21ab91c18fbdc32366f5
Monica Miller <33696269+monimiller@users.noreply.github.com>
2022-10-16 15:35:14 -0500

    create tpch int

---
+++ models/marts/intermediate/int_tpch/int_tpch_location.sql
@@ -0,0 +1,28 @@
+{{ config(tags = ['location']) }}
+
+with nation as (
+    
+    select * from {{ ref('stg_tpch_nations') }}
+
+),
+region as (
+    
+    select * from {{ ref('stg_tpch_regions') }}
+
+),
+final as (
+    nation.nation_key,
+    nation.name,
+    nation.region_key,
+    region.name
+    from
+        nation
+        inner join region
+            on nation.region_key = region.region_key
+)
+select
+    *
+from
+    final
+order by
+    nation_key
