0064345a686f8baf4dd83ef51f03b339b093d9c5
Anders Swanson <anders.swanson@dbtlabs.com>
2023-01-30 20:15:05 -0500

    dbt_packages dir should not be committed

--- dbt_packages/dbt_external_tables/macros/plugins/redshift/helpers/dropif.sql
+++
@@ -1,9 +0,0 @@
-{% macro redshift__dropif(node) %}
-    
-    {% set ddl %}
-        drop table if exists {{source(node.source_name, node.name)}} cascade
-    {% endset %}
-    
-    {{return(ddl)}}
-
-{% endmacro %}
